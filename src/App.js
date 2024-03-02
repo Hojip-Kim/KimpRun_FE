3ec7e27958693768209aff727e3ec2363eb79780
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-02 13:18:48 +0900

    feat: app.js test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
+  // <---- 자바스크립트 영역 ---->
+  const handleClick = () => {
+    alert('경고창입니다');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /* <---- HTML/JSX 영역  ---->*/
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
+      <a>이것은 내가 만든 App컴포넌트입니다.</a>
+      <button onClick={handleClick}>클릭!</button>
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+
+//   const handleClick = () => {
+//     alert('클릭!');
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}
+//     >
+//       <span>이것은 내가 만든 App컴포넌트 입니다</span>
+//       <button onClick={handleClick}>클릭!</button>
+//     </div>
+//   );
+// }
+
+// export default App;
