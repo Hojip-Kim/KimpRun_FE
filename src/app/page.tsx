f089ce3401f69f5779809112cdf8a3d9eb90804b
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-03 13:12:13 +0900

    feat : 서버 단 추가적 마켓 구현에 따른 redux 마켓데이터 적재 구현 및 fetch 로직 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,91 @@
 'use client';
 
 import Row from '@/components/Row';
-import { setTokenDataset, setTokenList } from '@/redux/redux';
+import {
+  setTokenFirstList,
+  setTokenFirstDataset,
+  setTokenSecondList,
+  setTokenSecondDataset,
+} from '@/redux/reducer/tokenReducer';
 import { AppDispatch, RootState } from '@/redux/store';
 import serverFetch from '@/server/fetch/server';
-import Script from 'next/script';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './page.css';
 import TradingViewWidget from '@/components/TradingViewWidget';
+import { error } from 'console';
 
-export const UpbitWebSocket = () => {
+export type TokenNameList = {
+  firstMarketData: any;
+  secondMarketData: any;
+};
+
+export type tokenNameList = {
+  firstMarketList: string[];
+  secondMarketList: string[];
+};
+
+const UpbitWebSocket = () => {
   // state
-  const [tokenNameList, setTokenNameList] = useState({});
-  const [tokenDataList, setTokenDataList] = useState({});
-  const [dataset, setDataset] = useState<{ [key: string]: any }>({});
+
+  const [firstDataset, setFirstDataset] = useState<{ [key: string]: any }>({});
+  const [secondDataset, setSecondDateset] = useState<{ [key: string]: any }>(
+    {}
+  );
 
   // redux
   const dispatch: AppDispatch = useDispatch();
-  const tokenList = useSelector((state: RootState) => state.token.tokenList);
-  const tokenSet = useSelector((state: RootState) => state.token.tokenSet);
-  const updateTokenList = (newTokenList) => {
-    dispatch(setTokenList(newTokenList));
+  const tokenFirstList = useSelector(
+    (state: RootState) => state.token.tokenList.first
+  );
+  const tokenSecondList = useSelector(
+    (state: RootState) => state.token.tokenList.second
+  );
+
+  const tokenFirstSet = useSelector(
+    (state: RootState) => state.token.tokenSet.first
+  );
+
+  const tokenSecondSet = useSelector(
+    (state: RootState) => state.token.tokenSet.second
+  );
+
+  const updateTokenFirstList = (newTokenList) => {
+    dispatch(setTokenFirstList(newTokenList));
   };
-  const updateTokenDataSet = (newTokenSet) => {
-    dispatch(setTokenDataset(newTokenSet));
+  const updateTokenFirstDataSet = (newTokenSet) => {
+    dispatch(setTokenFirstDataset(newTokenSet));
   };
 
-  const marketDataURL = process.env.NEXT_PUBLIC_MARKET_FIRST_DATA;
+  const updateTokenSecondList = (newTokenList) => {
+    dispatch(setTokenSecondList(newTokenList));
+  };
+
+  const updateTokenSecondDataSet = (newTokenSet) => {
+    dispatch(setTokenSecondDataset(newTokenSet));
+  };
+
+  const upbitMarketDataURL = process.env.NEXT_PUBLIC_MARKET_UPBIT_DATA;
+
+  const marketDataURL = process.env.NEXT_PUBLIC_MARKET_DATA;
+
   const marketListURL = process.env.NEXT_PUBLIC_MARKET_FIRST_NAME;
+
+  const upbitWebsocketURL = process.env.NEXT_PUBLIC_UPBIT_WEBSOCKET_URL;
+  const binanceWebsocketURL = process.env.NEXT_PUBLIC_BINANCE_WEBSOCKET_URL;
 
   const requestInit: RequestInit = {
     method: 'GET',
     headers: { 'Content-type': 'application/json' },
   };
 
-  const fetchTokenNames = async () => {
+  const fetchTokenNames = async (): Promise<tokenNameList | null> => {
     try {
       const nameList = await serverFetch(marketListURL, requestInit);
+
       if (nameList.ok) {
-        return nameList.text;
+        const text: tokenNameList = JSON.parse(nameList.text);
+        return text;
       } else {
         throw new Error('Data Name parse Error Occured!');
       }
@@ -49,9 +95,30 @@ export const UpbitWebSocket = () => {
     }
   };
 
-  const fetchTokenDatas = async () => {
+  const fetchTokenCombinedDatas = async (firstMarket: string, secondMarket) => {
     try {
-      const result = await serverFetch(marketDataURL, requestInit);
+      const url = new URL(marketDataURL);
+      url.searchParams.set('first', firstMarket);
+      url.searchParams.set('second', secondMarket);
+
+      const result = await serverFetch(url.toString(), requestInit);
+      if (result.ok) {
+        return JSON.parse(result.text);
+      } else {
+        throw new Error('Error Occured');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchTokenDatas = async (market: string) => {
+    try {
+      const url = new URL(upbitMarketDataURL);
+      url.searchParams.set('market', market);
+
+      // console.log(url.toString());
+      const result = await serverFetch(url.toString(), requestInit);
       if (result.ok) {
         return result.text;
       } else {
@@ -65,23 +132,39 @@ export const UpbitWebSocket = () => {
 
   const updateNamesAsync = async () => {
     try {
-      const tokenNames = await fetchTokenNames();
+      const tokenNames: tokenNameList = await fetchTokenNames();
+
       if (tokenNames) {
-        updateTokenList(tokenNames);
-        setTokenNameList(tokenNames);
+        updateTokenFirstList(tokenNames.firstMarketList);
+        updateTokenSecondList(tokenNames.secondMarketList);
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  const updateDataAsync = async () => {
+  const updateDataAsync = async (market: string, secondMarket?: string) => {
     try {
-      const tokenData = await fetchTokenDatas();
-
-      if (tokenData) {
-        updateTokenDataSet(tokenData);
-        setTokenDataList(tokenData);
+      let tokenData;
+      if (secondMarket === null || secondMarket === undefined) {
+        // 이 데이터는 사용되지 않을 데이터입니다.
+        tokenData = await fetchTokenDatas(market);
+        tokenData = JSON.parse(tokenData);
+        console.log(tokenData);
+        if (tokenData) {
+          if (market === 'upbit') {
+            await updateTokenFirstDataSet(tokenData);
+          } else if (market === 'binance') {
+            await updateTokenSecondDataSet(tokenData);
+          }
+        }
+      } else {
+        tokenData = await fetchTokenCombinedDatas(market, secondMarket);
+        if (tokenData) {
+      
+          await updateTokenFirstDataSet(tokenData.firstMarketDataList);
+          await updateTokenSecondDataSet(tokenData.secondMarketDataList);
+        }
       }
     } catch (error) {
       console.error(error);
@@ -90,16 +173,24 @@ export const UpbitWebSocket = () => {
 
   useEffect(() => {
     updateNamesAsync(); // 토큰이름 fetch
-    updateDataAsync(); // 토큰데이터 fetch
+    updateDataAsync('upbit', 'binance'); // 토큰데이터 fetch
+  }, []);
 
-    const ws = new WebSocket(process.env.NEXT_PUBLIC_WEBSOCKET_URL);
+  useEffect(() => {
+    if (tokenSecondSet) {
+      console.log(tokenSecondSet);
+    }
+  }, [tokenSecondSet]);
+
+  useEffect(() => {
+    const wsUpbit = new WebSocket(upbitWebsocketURL);
 
     // websocket 실시간 마켓데이터 호출
-    ws.onmessage = (event) => {
+    wsUpbit.onmessage = (event) => {
       const parsedData = JSON.parse(event.data);
 
-      if (tokenList) {
-        setDataset((prevState) => {
+      if (tokenFirstList) {
+        setFirstDataset((prevState) => {
           const newState = { ...prevState };
           Object.entries(parsedData).map(([key, value]) => {
             newState[key] = value;
@@ -110,12 +201,44 @@ export const UpbitWebSocket = () => {
       }
     };
 
-    ws.onerror = (error) => {
-      console.error('Websocket Error:', error);
-      ws.close();
+    wsUpbit.onerror = (error) => {
+      console.error('Upbit Websocket Error:', error);
+      wsUpbit.close();
+    };
+    // binance
+    const wsBinance = new WebSocket(binanceWebsocketURL);
+
+    // websocket 실시간 마켓데이터 호출
+    wsBinance.onmessage = (event) => {
+      const parsedData = JSON.parse(event.data);
+      console.log(parsedData);
+
+      if (tokenSecondList) {
+        setSecondDateset((prevState) => {
+          const newState = { ...prevState };
+          Object.entries(parsedData).map(([key, value]) => {
+            newState[key] = value;
+          });
+          return newState;
+        });
+      }
+
+      wsBinance.onerror = (error) => {
+        console.error('Binacne Websocket Error: ', error);
+        wsBinance.close();
+      };
+
     };
 
-    return () => ws.close();
+    wsBinance.onerror = (error) => {
+      console.error('Websocket Error:', error);
+      wsBinance.close();
+    };
+
+    return () => {
+      wsUpbit.close();
+      wsBinance.close();
+    };
   }, []);
 
   const dataTypes: String[] = [
@@ -149,10 +272,13 @@ export const UpbitWebSocket = () => {
       <div className="row_container">
         <Row
           title={dataTitle}
-          tokenNameList={tokenNameList.marketNameList}
-          tokenDataList={tokenDataList.marketDataList}
+          firstTokenNameList={tokenFirstList}
+          secondTokenNameList={tokenSecondList}
+          firstTokenDataList={tokenFirstSet}
+          secondTokenDataList={tokenSecondSet}
           dataTypes={dataTypes}
-          dataset={dataset}
+          firstDataset={firstDataset}
+          secondDataset={secondDataset}
         />
       </div>
 
