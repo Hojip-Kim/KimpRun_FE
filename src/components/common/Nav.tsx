f3733957aa64e787bb1f5bcfefc5ec907050c709
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-03 13:14:46 +0900

    feat : tether 관련 로직 추가 및 로고 클릭시 웹페이지 redirect

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -3,35 +3,39 @@
 import serverFetch from '@/server/fetch/server';
 import React, { useEffect, useState } from 'react';
 import './Nav.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '@reduxjs/toolkit/query';
 
 const Nav = () => {
   const [dollars, setDollars] = useState('');
-  const [tether, setTether] = useState('');
   const [userCount, setUserCount] = useState(0);
+
+  const reduxTether = useSelector((state: RootState) => state.info.tether);
 
   // TODO : refactoring - server side, client side hooks 나누기.
 
-  const GetTether = async (): Promise<string | undefined> => {
-    const URL = process.env.NEXT_PUBLIC_TETHER_URL;
+  // const GetTether = async (): Promise<string | undefined> => {
+  //   const URL = process.env.NEXT_PUBLIC_TETHER_URL;
 
-    const requestInit: RequestInit = {
-      method: 'GET',
-      headers: { 'Content-type': 'application/json' },
-    };
+  //   const requestInit: RequestInit = {
+  //     method: 'GET',
+  //     headers: { 'Content-type': 'application/json' },
+  //   };
 
-    try {
-      const response = await serverFetch(URL, requestInit);
+  //   try {
+  //     const response = await serverFetch(URL, requestInit);
 
-      if (response.ok) {
-        return await response.text;
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  //     if (response.ok) {
+  //       return await response.text;
+  //     }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   useEffect(() => {
-    GetTether().then((res) => setDollars(res[0].basePrice));
+    // GetTether().then((res) => setDollars(res[0].basePrice));
+    // setTether(reduxTether)
 
     setUserCount((prevCount) => prevCount + 1);
 
@@ -49,16 +53,21 @@ const Nav = () => {
             </div>
             <div className="nav-category">
               <img src="" />
-              테더 : {tether}{' '}
+              테더 : {reduxTether}{' '}
             </div>
-            <div className="user-count">{userCount}</div>
+            <div className="user-count">유저 수 : {userCount}</div>
           </div>
         </section>
         <section className="nav-container2">
           <div className="nav-category-2">
-            <a href="localhost:3000" className="title">
+            <button
+              onClick={() => {
+                window.location.href = 'http://localhost:3000/';
+              }}
+              className="title"
+            >
               KIMP-RUN
-            </a>
+            </button>
           </div>
         </section>
         {/* <div className="nav-container2"></div> */}
