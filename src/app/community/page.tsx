76bc2e9ae407dfdb91054d28438d74f397fc8a87
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-13 13:47:12 +0900

    WIP : 도메인 페이지 분리 진행

---
+++ src/app/community/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CommunityPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="title"></div>
+        <div className="board"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CommunityPage;
