8adbed239f86ebdbf71a946c11795e7a7035c491
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-29 23:57:18 +0900

    refactor : login로직 쿠키 핸들링을 위한 fetch로직 server component -> client componetn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,39 @@
 'use client';
 import { use, useState } from 'react';
 import './page.css';
-import { fetchLoginData } from './components/server/fetchData';
 
 const LoginPage = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  const fetchLoginData = async (
+    loginId: string,
+    password: string
+  ): Promise<boolean> => {
+    const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL;
+
+    const requestInit: RequestInit = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include', // 쿠키 포함
+      body: JSON.stringify({
+        loginId,
+        password,
+      }),
+    };
+
+    try {
+      const response = await fetch(loginUrl, requestInit);
+      if (response.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  };
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
