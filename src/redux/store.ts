f331ff76cde2c49914bc849e19029b12a522506d
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-19 01:20:16 +0900

    feat : redux persist 도입으로인한 redux store 변경

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,61 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import tokenReducer from './reducer/tokenReducer';
 import infoReducer from './reducer/infoReducer';
 import widgetReducer from './reducer/widgetReduce';
+import authReducer from './reducer/authReducer';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window === 'undefined'
+    ? createNoopStorage()
+    : createWebStorage('local');
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+export const rootReducer = combineReducers({
+  auth: authReducer,
+  token: tokenReducer,
+  info: infoReducer,
+  widget: widgetReducer,
+});
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    token: tokenReducer,
-    info: infoReducer,
-    widget: widgetReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
