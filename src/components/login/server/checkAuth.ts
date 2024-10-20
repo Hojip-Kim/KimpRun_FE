d598e2cb28d1ac3147970072fa338c5e82dcdeb9
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-20 23:37:30 +0900

    refactor : 로그인을 하든, 하지않든 user을 set (필드 내부값이 달라질 뿐, user 속성을 동등하게 가져가고, 이를통해 localStorage에 저장된 데이터를 토대로 채팅 분간을 user의 name으로 합니다.)

--- src/components/login/server/checkAuth.ts
+++ src/components/login/server/checkAuth.ts
@@ -1,5 +1,8 @@
-import { logout, setIsAuthenticated } from '@/redux/reducer/authReducer';
-import { setUser } from '@/redux/reducer/infoReducer';
+import {
+  logout,
+  setIsAuthenticated,
+  setUser,
+} from '@/redux/reducer/authReducer';
 import { AppDispatch } from '@/redux/store';
 
 const statusUrl = process.env.USER_STATUS_URL;
@@ -11,24 +14,16 @@ const reuqestInit: RequestInit = {
 
 export const checkAuth = async (dispatch: AppDispatch) => {
   try {
-    const response = await fetch(
-      statusUrl,
-      reuqestInit
-    );
-    if (response.ok) {
-      const data = await response.json();
-      // response data의 isAuthenticated키값이 true라면
-      if (data.isAuthenticated) {
-        dispatch(setIsAuthenticated(true));
-        dispatch(setUser(data.user));
-      } else {
-        // 현재 isAuthenticated 전역상태필드가 false면 로그아웃
-        dispatch(logout());
-      }
+    const response = await fetch(statusUrl, reuqestInit);
+    const responseJson = await response.json();
+
+    if (responseJson.isAuthenticated === true) {
+      await dispatch(setIsAuthenticated());
     } else {
       // 현재 유저의 세션쿠키가 비정상 쿠키라면 로그아웃
-      dispatch(logout());
+      await dispatch(logout());
     }
+    await dispatch(setUser(responseJson.user));
   } catch (error) {
     console.error('인증상태 확인 실패', error);
     dispatch(logout());
