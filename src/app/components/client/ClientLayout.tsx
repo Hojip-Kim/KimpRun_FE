e900c53acd6ef61038062e625fd2a6acffe9a988
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-25 23:48:31 +0900

    refactor : 전체 content 레이아웃 수정 (layout단에서의 수정)

--- src/app/components/client/ClientLayout.tsx
+++ src/app/components/client/ClientLayout.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 import store, { rootReducer } from '@/redux/store';
-import Nav from '../../../components/layout/nav/Nav';
+import Nav from '../../../components/nav/Nav';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import styled from 'styled-components';
@@ -19,16 +19,26 @@ const ClientLayout: React.FC<{ children: React.ReactNode }> = ({
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <html lang="en">
-          <Nav />
-          <BodyContatiner>{children}</BodyContatiner>
+          <LayoutWrapper>
+            <Nav />
+            <MainContent>{children}</MainContent>
+          </LayoutWrapper>
         </html>
       </PersistGate>
     </Provider>
   );
 };
 
-const BodyContatiner = styled.div`
-  margin-top: 100px;
+const LayoutWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  padding-top: 130px; // Nav의 높이와 동일하게 설정
+  min-height: calc(100vh - 130px); // 전체 높이에서 Nav 높이를 뺀 만큼
 `;
 
 export default ClientLayout;
