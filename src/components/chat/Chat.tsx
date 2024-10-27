c842fb6dd6a95386eae4336b2a87e108a9673fe3
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-27 23:28:38 +0900

    [#6] refactor : chat component server | client component 분리
    - 로그인을 하지 않아도 채팅 가능
    - NoSQL 도입 완료

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,97 +2,85 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import './Chat.css';
-import serverFetch from '@/server/fetch/server';
 import { useSelector } from 'react-redux';
+import { ChatMessage } from '@/types';
 import { RootState } from '@/redux/store';
-
-// const type Message = {
-
-// }
-
-interface ChatMessage {
-  chatID: string;
-  content: string;
-}
+import { fetchPreviousMessages } from './server/fetchPreviousMessages';
+import { Icon } from '../nav/client/styled';
 
 const Chat = () => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
   const [input, setInput] = useState<string>('');
   const [ws, setWs] = useState<WebSocket | null>(null);
   const [page, setPage] = useState<number>(0);
-
-  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [chatId, setChatId] = useState<string>('');
 
   const chatBoxRef = useRef<HTMLDivElement | null>(null);
   const messageEndRef = useRef<HTMLDivElement | null>(null);
 
   const websocketUrl = process.env.NEXT_PUBLIC_CHAT_WEBSOCKET_URL;
-  const getChatLogUrl = process.env.NEXT_PUBLIC_CHAT_LOG_URL;
 
-  const requestInit: RequestInit = {
-    method: 'GET',
-    headers: { 'Content-type': 'application/json' },
-  };
-
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
   const user = useSelector((state: RootState) => state.auth.user);
 
   const fetchPreviousMessage = async () => {
     try {
       if (chatBoxRef.current) {
-        const prev = chatBoxRef.current.scrollHeight;
-        const data = await serverFetch(
-          `${getChatLogUrl}?page=${page}&size=30`,
-          requestInit
-        );
+        const scrollContainer = chatBoxRef.current;
+        const prevScrollHeight = scrollContainer.scrollHeight;
+        const prevScrollTop = scrollContainer.scrollTop;
 
-        if (data.ok) {
-          const text: string = data.text;
-          const parsedData: ChatMessage[] = JSON.parse(text);
-          setMessages((prev) => [...parsedData, ...prev]);
-          setPage((prev) => prev + 1);
+        const parsedData = await fetchPreviousMessages(page, 30);
+        setMessages((prev) => [...parsedData, ...prev]);
+        setPage((prev) => prev + 1);
 
-          setTimeout(() => {
-            if (chatBoxRef.current) {
-              const newScrollHeight = chatBoxRef.current.scrollHeight;
-              chatBoxRef.current.scrollTop =
-                newScrollHeight - prev + chatBoxRef.current.scrollTop;
-            }
-          }, 500);
-        } else {
-          throw new Error('data fetching error occurred');
-        }
+        setTimeout(() => {
+          if (scrollContainer) {
+            const newScrollHeight = scrollContainer.scrollHeight;
+            scrollContainer.scrollTop =
+              newScrollHeight - prevScrollHeight + prevScrollTop;
+          }
+        }, 0);
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  type ChatMessage = {
-    chatID: string;
-    content: string;
-  };
-
   const handleSendMessage = () => {
     if (input.trim() && ws?.readyState === WebSocket.OPEN) {
-      const userNickname = user.name;
-      const message: ChatMessage = { chatID: userNickname, content: input };
+      const message: ChatMessage = {
+        chatID: chatId,
+        content: input,
+        authenticated: user.role === 'GUEST' ? 'false' : 'true',
+      };
+      console.log(message);
 
       ws.send(JSON.stringify(message));
       setInput('');
     } else {
-      console.error('message send error');
+      console.error('메시지 전송 오류: 웹소켓이 연결되지 않았습니다.');
     }
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
       handleSendMessage();
     }
   };
 
-  const handleScroll = () => {};
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   useEffect(() => {
+    const tempId = user.name;
+
+    setChatId(tempId);
+
     fetchPreviousMessage();
     const websocket = new WebSocket(websocketUrl);
 
@@ -102,30 +90,29 @@ const Chat = () => {
 
     websocket.onmessage = (event) => {
       const parsedData: ChatMessage = JSON.parse(event.data);
-
       if (parsedData) {
         setMessages((prev) => [...prev, parsedData]);
       }
     };
 
     websocket.onerror = (error) => {
-      console.error('Chatting Websocket Error : ', error);
+      console.error('채팅 웹소켓 오류: ', error);
       websocket.close();
     };
 
     return () => websocket.close();
-  }, []);
+  }, [user]);
 
   useEffect(() => {
-    if (page === 1) {
-      messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (page > 0) {
+      scrollToBottom();
     }
   }, [messages, page]);
 
   return (
     <div>
       <div className="chat-container">
-        <div className="chat-box" ref={chatBoxRef} onScroll={handleScroll}>
+        <div className="chat-box" ref={chatBoxRef}>
           <div
             className="fetch-button"
             onClick={() => {
@@ -135,30 +122,50 @@ const Chat = () => {
             이전 메세지 불러오기
           </div>
           {messages.map((data, index) => (
-            <div key={index}>
+            <div
+              key={index}
+              style={{
+                color: data.authenticated === 'true' ? 'black' : 'white',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {data.authenticated === 'true' && (
+                <Icon
+                  src="/bitcoin.png"
+                  alt="medal"
+                  style={{ marginRight: '5px' }}
+                />
+              )}
               {data.chatID} : {data.content}
             </div>
           ))}
           <div ref={messageEndRef}></div>
         </div>
       </div>
-      <div className="chat-typing-box">
-        <input
-          type="text"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          onKeyDown={handleKeyDown}
-          placeholder="채팅 메시지를 입력하세요."
-        ></input>
-        <button
-          onClick={() => {
-            handleSendMessage();
-          }}
-          style={{ color: 'white' }}
-        >
-          Send
-        </button>
-      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSendMessage();
+        }}
+      >
+        <div className="chat-typing-box">
+          <input
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onKeyDown={(e) =>
+              e.key === 'Enter' && e.nativeEvent.isComposing === false
+                ? handleKeyPress(e)
+                : null
+            }
+            placeholder="채팅 메시지를 입력하세요."
+          />
+          <button type="submit" style={{ color: 'black' }}>
+            전송
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
