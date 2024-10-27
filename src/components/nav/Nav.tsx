e6f3b3a23499f5a9fa2974d2f3d118bf5da503a5
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-27 23:24:56 +0900

    [#16] feat : 커뮤니티 (게시판) 네비게이션 바 구현 및 styled component 분리

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,24 +1,46 @@
 'use client';
 
-import React, { useCallback, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import store, { AppDispatch, RootState } from '@/redux/store';
+import { AppDispatch, RootState } from '@/redux/store';
 import Modal from '@/components/modal/modal';
 import LoginForm from '@/components/login/loginForm';
-import {
-  logout,
-  setIsAuthenticated,
-  setUser,
-} from '@/redux/reducer/authReducer';
+import { logout } from '@/redux/reducer/authReducer';
 import { fetchUserInfo } from '@/components/auth/fetchUserInfo';
 import ProfileForm from '../profile/ProfileForm';
+import {
+  ActionButton,
+  ActionButtons,
+  BottomSection,
+  CloseButton,
+  Icon,
+  InfoContainer,
+  InfoItem,
+  Logo,
+  NavbarWrapper,
+  NavMenu,
+  NavMenuItem,
+  NavMenuLink,
+  SubMenu,
+  SubMenuItem,
+  TopSection,
+  UserContainer,
+  UserName,
+  UserRole,
+  UserWrapperContainer,
+} from './client/styled';
+import { useRouter } from 'next/navigation';
+
+interface ResponseUrl {
+  response: string;
+}
 
 const Nav = () => {
   const [dollars, setDollars] = useState('');
   const [userCount, setUserCount] = useState(0);
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
   const [modalSize, setModalSize] = useState({ width: 400, height: 300 });
+  const router = useRouter();
 
   const reduxTether = useSelector((state: RootState) => state.info.tether);
   const isAuthenticated = useSelector(
@@ -29,9 +51,10 @@ const Nav = () => {
 
   const statusUrl = process.env.NEXT_PUBLIC_STATUS_URL;
   const logoutUrl = process.env.NEXT_PUBLIC_LOGOUT_URL;
+  const adminUrl = process.env.NEXT_PUBLIC_ADMIN_URL;
 
   const checkUserAuth = async () => {
-    if (!isAuthenticated) {
+    if (isAuthenticated) {
       await fetchUserInfo(statusUrl, dispatch);
     }
   };
@@ -72,9 +95,25 @@ const Nav = () => {
     setIsModalActive(true);
   };
 
-  const handleCateogryClick = () => {
-    setModalSize({ width: 400, height: 700 });
-    setIsModalActive(true);
+  const handleAdminClick = async () => {
+    try {
+      const requestInit: RequestInit = {
+        method: 'GET',
+        credentials: 'include',
+      };
+      const response = await fetch(adminUrl, requestInit);
+      if (response.ok) {
+        const redirectUrl: ResponseUrl = await response.json();
+        if (redirectUrl) {
+          window.location.href = redirectUrl.response;
+        } else {
+          console.error('리다이렉션 URL을 찾을 수 없음.');
+        }
+      }
+    } catch (error) {
+      console.error('카테고리 페이지 접근 오류', error);
+      alert('카테고리 페이지 접근 중 오류 발생');
+    }
   };
 
   const handleNickname = () => {
@@ -84,7 +123,7 @@ const Nav = () => {
     if (nickname) {
       localStorage.setItem('guestName', nickname);
     }
-    window.location.reload();
+    router.refresh();
   };
 
   return (
@@ -92,6 +131,13 @@ const Nav = () => {
       <TopSection>
         <InfoContainer>
           <InfoItem>
+            <Logo
+              onClick={() =>
+                (window.location.href = process.env.NEXT_PUBLIC_MAIN_PAGE)
+              }
+            >
+              KIMP-RUN
+            </Logo>
             <Icon src="/dollar.png" alt="Dollar" />
             환율: {dollars}
           </InfoItem>
@@ -101,16 +147,16 @@ const Nav = () => {
           </InfoItem>
           <InfoItem>유저 수: {userCount}</InfoItem>
         </InfoContainer>
-        <UserContainer>
-          <UserRole role={user?.role}>
-            {isAuthenticated ? user?.role : '로그인 되지 않은 사용자'}
-          </UserRole>
-          <UserName>{`안녕하세요. ${user?.name}님`}</UserName>
+        <UserWrapperContainer>
+          <UserContainer>
+            <UserRole role={user?.role}>
+              {isAuthenticated ? user?.role : '로그인 되지 않은 사용자'}
+            </UserRole>
+            <UserName>{`안녕하세요. ${user?.name}님`}</UserName>
+          </UserContainer>
           <ActionButtons>
             {isAuthenticated && user?.role === 'OPERATOR' && (
-              <ActionButton onClick={handleCateogryClick}>
-                카테고리 생성
-              </ActionButton>
+              <ActionButton onClick={handleAdminClick}>어드민</ActionButton>
             )}
             {isAuthenticated && (
               <ActionButton onClick={handleProfileClick}>
@@ -128,23 +174,34 @@ const Nav = () => {
               {isAuthenticated ? '로그아웃' : '로그인'}
             </ActionButton>
           </ActionButtons>
-        </UserContainer>
+        </UserWrapperContainer>
       </TopSection>
       <BottomSection>
-        <Logo
-          onClick={() =>
-            (window.location.href = process.env.NEXT_PUBLIC_MAIN_PAGE)
-          }
-        >
-          KIMP-RUN
-        </Logo>
         <NavMenu>
-          <NavMenuItem
-            onClick={() =>
-              (window.location.href = process.env.NEXT_PUBLIC_COMMUNITY_PAGE)
-            }
-          >
-            커뮤니티
+          <NavMenuItem>
+            <NavMenuLink
+              onClick={() =>
+                (window.location.href = process.env.NEXT_PUBLIC_COMMUNITY_PAGE)
+              }
+            >
+              커뮤니티
+            </NavMenuLink>
+            <SubMenu>
+              <SubMenuItem
+                onClick={() =>
+                  (window.location.href = `${process.env.NEXT_PUBLIC_COMMUNITY_PAGE}/expert`)
+                }
+              >
+                전문가 게시판
+              </SubMenuItem>
+              <SubMenuItem
+                onClick={() =>
+                  (window.location.href = `${process.env.NEXT_PUBLIC_COMMUNITY_PAGE}/coin`)
+                }
+              >
+                코인 게시판
+              </SubMenuItem>
+            </SubMenu>
           </NavMenuItem>
           <NavMenuItem
             onClick={() =>
@@ -193,129 +250,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-// Styled Components
-const NavbarWrapper = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 130px; // 네비게이션 바의 높이
-  z-index: 1000;
-  background-color: white;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const TopSection = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 20px;
-  border-bottom: 1px solid #e0e0e0;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const InfoItem = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 20px;
-  font-size: 0.9rem;
-`;
-
-const Icon = styled.img`
-  width: 20px;
-  height: 20px;
-  margin-right: 5px;
-`;
-
-const UserContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`;
-
-const UserRole = styled.div<{ role?: string }>`
-  color: ${(props) =>
-    props.role === 'OPERATOR' || props.role === 'MANAGER' ? 'red' : 'black'};
-  font-weight: ${(props) =>
-    props.role === 'OPERATOR' || props.role === 'MANAGER' ? 'bold' : 'normal'};
-  margin-bottom: 5px;
-`;
-
-const UserName = styled.div`
-  font-size: 0.9rem;
-  margin-bottom: 5px;
-`;
-
-const ActionButtons = styled.div`
-  display: flex;
-`;
-
-const ActionButton = styled.button`
-  background-color: #f0f0f0;
-  border: none;
-  padding: 5px 10px;
-  margin-left: 10px;
-  cursor: pointer;
-  border-radius: 4px;
-  font-size: 0.8rem;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-`;
-const BottomSection = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px 20px;
-  height: 0px;
-`;
-
-const Logo = styled.div`
-  font-size: 1.5rem;
-  font-weight: bold;
-  cursor: pointer;
-  margin-right: 30px;
-  display: flex;
-  align-items: center;
-  height: 100%;
-`;
-
-const NavMenu = styled.ul`
-  display: flex;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  height: 100%;
-  align-items: center;
-`;
-
-const NavMenuItem = styled.li`
-  margin-right: 20px;
-  cursor: pointer;
-  font-size: 1rem;
-  height: 100%; // 부모 높이에 맞춤
-  display: flex;
-  align-items: center; // 세로 중앙 정렬
-
-  &:hover {
-    color: #007bff;
-  }
-`;
-
-const CloseButton = styled.button`
-  background-color: #f0f0f0;
-  border: none;
-  padding: 5px 10px;
-  margin-top: 10px;
-  cursor: pointer;
-  border-radius: 4px;
-  font-size: 0.8rem;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-`;
