9f8da06127b0ade0c997aca853bce80d62e3f42a
Hojip-Kim <rlaghwlq12@naver.com>
2024-11-07 23:09:30 +0900

    refactor : chat component styled component 적용, 로그인 여부에 따른 채팅UI 변경

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -7,6 +7,7 @@ import { ChatMessage } from '@/types';
 import { RootState } from '@/redux/store';
 import { fetchPreviousMessages } from './server/fetchPreviousMessages';
 import { Icon } from '../nav/client/styled';
+import styled from 'styled-components';
 
 const Chat = () => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
@@ -20,9 +21,6 @@ const Chat = () => {
 
   const websocketUrl = process.env.NEXT_PUBLIC_CHAT_WEBSOCKET_URL;
 
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
   const user = useSelector((state: RootState) => state.auth.user);
 
   const fetchPreviousMessage = async () => {
@@ -56,7 +54,6 @@ const Chat = () => {
         content: input,
         authenticated: user.role === 'GUEST' ? 'false' : 'true',
       };
-      console.log(message);
 
       ws.send(JSON.stringify(message));
       setInput('');
@@ -72,16 +69,11 @@ const Chat = () => {
     }
   };
 
-  const scrollToBottom = () => {
-    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
   useEffect(() => {
-    const tempId = user.name;
-
-    setChatId(tempId);
+    setChatId(user?.name);
 
     fetchPreviousMessage();
+
     const websocket = new WebSocket(websocketUrl);
 
     websocket.onopen = () => {
@@ -100,74 +92,152 @@ const Chat = () => {
       websocket.close();
     };
 
-    return () => websocket.close();
+    return () => {
+      websocket.close();
+    };
   }, [user]);
-
-  useEffect(() => {
-    if (page > 0) {
-      scrollToBottom();
-    }
-  }, [messages, page]);
 
   return (
     <div>
-      <div className="chat-container">
-        <div className="chat-box" ref={chatBoxRef}>
-          <div
-            className="fetch-button"
-            onClick={() => {
-              fetchPreviousMessage();
-            }}
-          >
+      <ChatContainer>
+        <ChatBox ref={chatBoxRef}>
+          <FetchButton onClick={fetchPreviousMessage}>
             이전 메세지 불러오기
-          </div>
+          </FetchButton>
           {messages.map((data, index) => (
-            <div
+            <MessageContainer
               key={index}
-              style={{
-                color: data.authenticated === 'true' ? 'black' : 'white',
-                display: 'flex',
-                alignItems: 'center',
-              }}
+              authenticated={data.authenticated}
+              isSelf={data.chatID === user?.name}
             >
-              {data.authenticated === 'true' && (
-                <Icon
-                  src="/bitcoin.png"
-                  alt="medal"
-                  style={{ marginRight: '5px' }}
-                />
-              )}
-              {data.chatID} : {data.content}
-            </div>
+              <div>
+                <MessageContent>
+                  {data.chatID === user?.name ? '' : data.chatID + ':'}{' '}
+                  {data.content}
+                </MessageContent>
+                {data.authenticated === 'true' &&
+                  data.chatID !== user?.name && (
+                    <Icon
+                      src="/bitcoin.png"
+                      alt="medal"
+                      style={{ margin: '0 5px' }}
+                    />
+                  )}
+              </div>
+            </MessageContainer>
           ))}
           <div ref={messageEndRef}></div>
-        </div>
-      </div>
-      <form
+        </ChatBox>
+      </ChatContainer>
+      <ChatForm
         onSubmit={(e) => {
           e.preventDefault();
           handleSendMessage();
         }}
       >
-        <div className="chat-typing-box">
-          <input
-            type="text"
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-            onKeyDown={(e) =>
-              e.key === 'Enter' && e.nativeEvent.isComposing === false
-                ? handleKeyPress(e)
-                : null
-            }
-            placeholder="채팅 메시지를 입력하세요."
-          />
-          <button type="submit" style={{ color: 'black' }}>
-            전송
-          </button>
-        </div>
-      </form>
+        <ChatInput
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          onKeyDown={(e) =>
+            e.key === 'Enter' && e.nativeEvent.isComposing === false
+              ? handleKeyPress(e)
+              : null
+          }
+          placeholder="채팅 메시지를 입력하세요."
+        />
+        <SendButton type="submit">전송</SendButton>
+      </ChatForm>
     </div>
   );
 };
 
 export default Chat;
+
+const ChatContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  color: #e0e0e0;
+  max-height: 700px;
+  overflow-y: auto;
+  background-color: #131722;
+  border-radius: 8px;
+  font-size: 0.7rem;
+  border: 2px solid rgba(123, 123, 123, 0.4);
+  padding: 5px;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const ChatBox = styled.div``;
+
+const FetchButton = styled.div`
+  cursor: pointer;
+  color: #4a90e2;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const MessageContainer = styled.div<{ authenticated: string; isSelf: boolean }>`
+  color: ${(props) => (props.authenticated === 'true' ? '#4a90e2' : '#e0e0e0')};
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+  width: 100%;
+  justify-content: ${(props) => (props.isSelf ? 'flex-end' : 'flex-start')};
+
+  & > div {
+    display: flex;
+    align-items: center;
+    flex-direction: ${(props) => (props.isSelf ? 'row' : 'row-reverse')};
+    max-width: 80%;
+  }
+`;
+
+const MessageContent = styled.span`
+  background-color: #2c2c2c;
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
+
+const ChatForm = styled.form`
+  display: flex;
+  margin-top: 10px;
+`;
+
+const ChatInput = styled.input`
+  flex-grow: 1;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #2c2c2c;
+  color: #e0e0e0;
+`;
+
+const SendButton = styled.button`
+  padding: 10px 20px;
+  margin-left: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #4a90e2;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3a7bc8;
+  }
+`;
