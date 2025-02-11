fb3fed9b3c490fc430d8458297e41f1a65c1c74b
Hojip-Kim <rlaghwlq12@naver.com>
2025-02-12 01:53:03 +0900

    refactor : Styled Component UI refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,28 +231,34 @@ const MainContainer = styled.div`
   display: flex;
   background-color: #121212;
   color: #ffffff;
-  height: calc(100vh - 150px)
+  height: 100vh;
   overflow: hidden;
+  padding-bottom: 150px;
+  gap: 20px;
+  justify-content: center;
 `;
 
 const LeftSideContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 150px)
+  height: calc(100vh - 130px);
   flex: 3;
   min-width: 400px;
+  max-width: 600px;
+  gap: 20px;
+  margin: 20px 0 20px 20px; // 전체 여백을 여기서 처리
 `;
 
 const ChartContainer = styled.div`
-  min-height: 0;
-  margin: 20px 0px 20px 20px;
+  flex: 1;
+  min-height: 300px;
+  max-height: 30%;
 
   #chart {
     justify-content: center;
     background-color: #131722;
     align-items: left;
-    height: 300px;
-    margin: auto;
+    height: 100%;
     width: 100%;
     border: 1px solid #333333;
     border-radius: 10px;
@@ -261,26 +267,30 @@ const ChartContainer = styled.div`
 `;
 
 const RowContainer = styled.div`
-  flex: 4;
-  min-width: 900px;
-  margin: 20px 20px 20px 20px;
-  flex: 1;
+  flex: 4.5;
+  min-width: 600px;
+  max-width: 900px;
+  margin: 20px 0;
   padding: 20px;
-  width: 55%;
   background-color: #1e1e1e;
   border-radius: 10px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  height: calc(100vh - 130px);
+  height: calc(100vh - 150px);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden; / 컨테이너 자체는 overflow 숨김
 `;
 
 const ChatContainer = styled.div`
-  flex: 2;
+  flex: 2.5;
   min-width: 300px;
+  max-width: 400px;
   background-color: #1e1e1e;
-  margin: 20px 20px 20px 0px;
+  margin: 20px 20px 20px 0;
   border-radius: 10px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   padding: 20px;
-  height: calc(100vh - 130px);
-  overflow-y: auto;
+  height: 100%;
+  display: flex; // 추가
+  flex-direction: column; // 추가
 `;
