c739b4491dc92cef0aa98d60828e80078bb2f90e
Hojip-Kim <rlaghwlq12@naver.com>
2025-02-12 02:09:49 +0900

    refactor : auth check compoentn type define, user -> member refactor

--- src/components/login/server/checkAuth.ts
+++ src/components/login/server/checkAuth.ts
@@ -4,18 +4,24 @@ import {
   setUser,
 } from '@/redux/reducer/authReducer';
 import { AppDispatch } from '@/redux/store';
+import { User } from '@/types';
 
-const statusUrl = process.env.USER_STATUS_URL;
+const statusUrl = process.env.NEXT_PUBLIC_STATUS_URL;
 
 const reuqestInit: RequestInit = {
   method: 'GET',
   credentials: 'include',
 };
 
+interface ResponseAuth {
+  isAuthenticated: boolean;
+  member: User;
+}
+
 export const checkAuth = async (dispatch: AppDispatch) => {
   try {
     const response = await fetch(statusUrl, reuqestInit);
-    const responseJson = await response.json();
+    const responseJson: ResponseAuth = await response.json();
 
     if (responseJson.isAuthenticated === true) {
       await dispatch(setIsAuthenticated());
@@ -23,7 +29,7 @@ export const checkAuth = async (dispatch: AppDispatch) => {
       // 현재 유저의 세션쿠키가 비정상 쿠키라면 로그아웃
       await dispatch(logout());
     }
-    await dispatch(setUser(responseJson.user));
+    await dispatch(setUser(responseJson.member));
   } catch (error) {
     console.error('인증상태 확인 실패', error);
     dispatch(logout());
