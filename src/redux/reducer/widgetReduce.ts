7fb8606eba383c5cb5bf665eb48db39b60d58b92
Hojip-Kim <rlaghwlq12@naver.com>
2025-02-12 02:17:12 +0900

    feat : widget field값 추가 (kimp, token price)

--- src/redux/reducer/widgetReduce.ts
+++ src/redux/reducer/widgetReduce.ts
@@ -2,7 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const widgetSlices = createSlice({
   name: 'widget',
-  initialState: { token: '', currency: 'KRW', interval: '240' },
+  initialState: {
+    token: '',
+    currency: 'KRW',
+    interval: '240',
+    tokenPrice: 0,
+    kimp: 0,
+  },
   reducers: {
     setToken: (state, action) => {
       state.token = action.payload;
@@ -13,8 +19,15 @@ const widgetSlices = createSlice({
     setInterval: (state, action) => {
       state.interval = action.payload;
     },
+    setTokenPrice: (state, action) => {
+      state.tokenPrice = action.payload;
+    },
+    setKimp: (state, action) => {
+      state.kimp = action.payload;
+    },
   },
 });
 
-export const { setToken, setCurrency, setInterval } = widgetSlices.actions;
+export const { setToken, setCurrency, setInterval, setTokenPrice, setKimp } =
+  widgetSlices.actions;
 export default widgetSlices.reducer;
