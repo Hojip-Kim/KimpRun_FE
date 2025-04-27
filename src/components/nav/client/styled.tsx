8d42a93159526c52dff206c3e118224b95343794
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-27 23:11:56 +0900

    refactor : nav bar UI 수정

--- src/components/nav/client/styled.tsx
+++ src/components/nav/client/styled.tsx
@@ -33,6 +33,22 @@ export const InfoItem = styled.div`
   align-items: center;
   margin-right: 20px;
   font-size: 0.9rem;
+
+  @media (max-width: 1200px) {
+    margin-right: 20px;
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 768px) {
+    margin-right: 15px;
+    font-size: 0.7rem;
+  }
+
+  @media (max-width: 480px) {
+    margin-right: 10px;
+    font-size: 0.6rem;
+  }
+
   color: #e0e0e0;
 `;
 export const TopInfoSection = styled.div`
@@ -41,33 +57,33 @@ export const TopInfoSection = styled.div`
 `;
 
 export const LogoIcon = styled.img`
-  width: 50px;
-  height: 50px;
-  min-width: 35px;
-  min-height: 35px;
+  width: 40px;
+  height: 40px;
+  min-width: 30px;
+  min-height: 30px;
 
   object-fit: contain;
   aspect-ratio: 1;
 
   @media (max-width: 1200px) {
-    width: 45px;
-    height: 45px;
-  }
-
-  @media (max-width: 768px) {
     width: 40px;
     height: 40px;
   }
 
-  @media (max-width: 480px) {
+  @media (max-width: 768px) {
     width: 35px;
     height: 35px;
+  }
+
+  @media (max-width: 480px) {
+    width: 30px;
+    height: 30px;
   }
 `;
 
 export const Icon = styled.img`
-  width: 20px;
-  height: 20px;
+  width: 15px;
+  height: 15px;
   margin-right: 5px;
 `;
 
@@ -130,7 +146,7 @@ export const Logo = styled.div`
   font-size: 1.5rem;
   font-weight: bold;
   cursor: pointer;
-  margin-right: 30px;
+  margin-right: 20px;
   display: flex;
   align-items: center;
   height: 100%;
@@ -143,7 +159,7 @@ export const NavMenu = styled.ul`
   margin: 0;
   height: 100%;
   align-items: center;
-  font-size: 0.9rem;
+
   color: #e0e0e0;
 `;
 
@@ -170,7 +186,7 @@ export const SubMenu = styled.ul`
   position: absolute;
   background-color: #2c2c2c;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-  margin-top: 30px; 
+  margin-top: 30px;
   z-index: 1;
   min-width: 150px;
   border-radius: 4px;
@@ -204,9 +220,8 @@ export const SubMenuItem = styled.li`
 `;
 
 export const NavMenuItem = styled.li`
-  margin-right: 20px;
   cursor: pointer;
-  font-size: 0.9rem;
+
   font-weight: 700;
   height: 100%;
   display: flex;
@@ -216,6 +231,23 @@ export const NavMenuItem = styled.li`
   padding-bottom: 20px;
   color: #e0e0e0;
   transition: color 0.3s ease;
+  margin-right: 40px;
+  font-size: 0.9rem;
+
+  @media (max-width: 1200px) {
+    margin-right: 30px;
+    font-size: 0.75rem;
+  }
+
+  @media (max-width: 768px) {
+    margin-right: 20px;
+    font-size: 0.7rem;
+  }
+
+  @media (max-width: 480px) {
+    margin-right: 10px;
+    font-size: 0.6rem;
+  }
 
   &:hover {
     color: #ffd700;
