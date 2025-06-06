5027c13759e3cc2a0eca21657558fd20b9c9b44c
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-06 18:36:56 +0900

    #28 feat: admin page 서버컴포넌트로 렌더링, styled component 별도파일 구성으로 분리, type 분리

--- src/app/admin/server/fetchCategory.ts
+++ src/app/admin/server/fetchCategory.ts
@@ -1,89 +1,107 @@
-import { clientEnv } from '@/utils/env';
+import { serverEnv } from '@/utils/env';
+import { ApiResponse, serverRequest } from '@/server/fetch';
+import { Category, CategoryResponse } from '../type';
 
-export type category = {
-  id: number;
-  categoryName: string;
-};
-
-export const createCategory = async (
-  categoryName: string
-): Promise<category> => {
-  const requestInit: RequestInit = {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  const response = await fetch(clientEnv.CATEGORY_URL, {
-    ...requestInit,
-    body: JSON.stringify({ categoryName }),
-  });
-
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error('Failed to fetch category');
-  }
-};
-
-export const getCategories = async (): Promise<category[]> => {
-  const requestInit: RequestInit = {
-    method: 'GET',
-    credentials: 'include',
-    cache: 'no-store',
-  };
-  const response = await fetch(clientEnv.CATEGORY_URL, {
-    ...requestInit,
-  });
-
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error('Failed to fetch categories');
-  }
-};
-
-export const updateCategory = async (
-  id: number,
-  categoryName: string
-): Promise<category> => {
-  const requestInit: RequestInit = {
-    method: 'PATCH',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ categoryId: id, categoryName }),
-  };
-  const response = await fetch(clientEnv.CATEGORY_URL, {
-    ...requestInit,
-  });
-
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error('Failed to update category');
-  }
-};
-
-export const deleteCategory = async (id: number): Promise<boolean> => {
+// 모든 카테고리 가져오기
+export async function fetchAllCategories(): Promise<
+  ApiResponse<CategoryResponse>
+> {
   try {
-    const requestInit: RequestInit = {
-      method: 'DELETE',
-      credentials: 'include',
-    };
-    const response = await fetch(`${clientEnv.CATEGORY_URL}/${id}`, {
-      ...requestInit,
-    });
-
-    if (response.status === 204) {
-      return true; // 삭제 성공
-    } else if (response.status === 404) {
-      return false; // 카테고리 없음
+    const response = await serverRequest.get<CategoryResponse>(
+      serverEnv.CATEGORY_URL,
+      {
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    if (response.success && response.data) {
+      return response;
+    } else {
+      console.error('카테고리 가져오기 실패:', response.error);
+      return null;
     }
   } catch (error) {
-    console.error('카테고리 삭제 중 오류 발생:', error);
-    throw new Error('카테고리 삭제 실패');
+    console.error('카테고리 요청 오류:', error);
+    return null;
   }
-};
+}
+
+// 카테고리 생성
+export async function createCategory(
+  name: string,
+  description: string
+): Promise<ApiResponse<Category>> {
+  try {
+    const response = await serverRequest.post<Category>(
+      serverEnv.CATEGORY_URL,
+      { name, description },
+      {
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    if (response.success && response.data) {
+      return response;
+    } else {
+      console.error('카테고리 생성 실패:', response.error);
+      return null;
+    }
+  } catch (error) {
+    console.error('카테고리 생성 오류:', error);
+    return null;
+  }
+}
+
+// 카테고리 수정
+export async function updateCategory(
+  id: number,
+  name: string,
+  description: string
+): Promise<ApiResponse<Category>> {
+  try {
+    const response = await serverRequest.put<Category>(
+      serverEnv.CATEGORY_URL,
+      { id, name, description },
+      {
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    if (response.success && response.data) {
+      return response;
+    } else {
+      console.error('카테고리 수정 실패:', response.error);
+      return null;
+    }
+  } catch (error) {
+    console.error('카테고리 수정 오류:', error);
+    return null;
+  }
+}
+
+// 카테고리 삭제
+export async function deleteCategory(
+  id: number
+): Promise<ApiResponse<Boolean>> {
+  try {
+    const response = await serverRequest.delete(
+      `${serverEnv.CATEGORY_URL}/${id}`,
+      {
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    if (response.success) {
+      return response;
+    } else {
+      console.error('카테고리 삭제 실패:', response.error);
+      return null;
+    }
+  } catch (error) {
+    console.error('카테고리 삭제 오류:', error);
+    return null;
+  }
+}
