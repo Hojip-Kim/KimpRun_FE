29f30a1934f4f295d8e5f6d3b8fdebe85ac5f8bd
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-06 19:12:51 +0900

    #28 feat : error message 컴포넌트(클라이언트 컴포넌트) 스타일컴포넌트 분리작업 완료

--- src/components/error/ErrorMessage.tsx
+++ src/components/error/ErrorMessage.tsx
@@ -1,35 +1,5 @@
 'use client';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 50vh;
-  text-align: center;
-  padding: 1rem;
-`;
-
-const Title = styled.h1`
-  color: white;
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-`;
-
-const MessageCard = styled.div`
-  background-color: #1f2937;
-  padding: 1.5rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  border: 1px solid #374151;
-`;
-
-const Message = styled.p`
-  color: #e5e7eb;
-  font-size: 1.125rem;
-`;
+import { Container, Title, MessageCard, Message } from './style';
 
 interface ErrorMessageProps {
   title: string;
