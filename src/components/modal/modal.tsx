3590cb15b2bbb7a1fc1322f09f292d5142cdd9cd
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-06 19:14:59 +0900

    #28 refactor : modal 컴포넌트 style컴포넌트만 분리작업 완료

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
+import { Canvas, Container, Wrapper } from './style';
 
 interface ModalProps {
   width: number;
@@ -26,49 +27,5 @@ const Modal: React.FC<ModalProps> = ({ width, height, element, setModal }) => {
     </>
   );
 };
-
-const Container = styled.div<{ width: number; height: number }>`
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-  padding: 8px;
-  background-color: black;
-  border-radius: 8px;
-  z-index: 2000;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
-  color: white;
-
-  .exit-wrapper {
-    position: absolute;
-    top: 4px;
-    right: 4px;
-    font-size: 14px;
-    font-weight: 800;
-    background-color: transparent;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-
-const Canvas = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.6);
-  z-index: 1000;
-`;
-
-const Wrapper = styled.div`
-  background-color: transparent;
-`;
 
 export default Modal;
