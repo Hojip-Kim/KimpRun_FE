c76b564ef36b4505214de0ce4997103a2b7c96c9
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-07 21:51:11 +0900

    feat : 공통 page관련 타입 지정

--- src/types/page.ts
+++ src/types/page.ts
@@ -2,3 +2,32 @@ export interface PageRequest {
   page: number;
   size: number;
 }
+
+export interface PageResponse<T> {
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  content: T[];
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  pageable: Pageable;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  unpaged: boolean;
+  paged: boolean;
+}
