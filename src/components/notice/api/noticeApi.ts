9b3f2f4231363bb5cb4265f2ec6ed98945f5e061
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-08 11:52:42 +0900

    #27 feat : server뿐이 아닌, client측 데이터페칭도 제공

--- src/components/notice/api/noticeApi.ts
+++ src/components/notice/api/noticeApi.ts
@@ -3,17 +3,24 @@ import { NoticeParameter } from '../type';
 
 export const noticeEndPoint = {
   getNotices: (parameter: NoticeParameter) => {
+    const marketTypeValue = Number(parameter.marketType);
+
+    // NaN 체크 및 기본값 설정
+    const finalMarketTypeValue = isNaN(marketTypeValue) ? 1 : marketTypeValue;
+
     const searchParameters = new URLSearchParams({
-      marketType: parameter.marketType.toString(),
+      marketType: finalMarketTypeValue.toString(),
       page: (parameter.page || 0).toString(),
       size: (parameter.size || 15).toString(),
     });
 
     return {
-      clientUrl: `${clientEnv.NOTICE_URL}/${parameter.marketType}?page=${parameter.page}&size=${parameter.size}`,
-      serverUrl: `${serverEnv.NOTICE_URL}/${
-        parameter.marketType
-      }?${searchParameters.toString()}`,
+      clientUrl: `${
+        clientEnv.NOTICE_URL
+      }/${finalMarketTypeValue}?${searchParameters.toString()}`,
+      serverUrl: `${
+        serverEnv.NOTICE_URL
+      }/${finalMarketTypeValue}?${searchParameters.toString()}`,
     };
   },
 };
