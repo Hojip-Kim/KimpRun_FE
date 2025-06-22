3ac336646494186e2ad9b6162fc677613794373d
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-22 12:17:25 +0900

    #45 feat : 메인 / 비교 거래소 변경에 따른 리렌더링 로직 추가

--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -1,16 +1,15 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '@/redux/store';
-import { setKimp, setToken, setTokenPrice } from '@/redux/reducer/widgetReduce';
-import { firstDataSet, secondDataSet } from '@/app/types';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { setKimp, setToken, setTokenPrice } from "@/redux/reducer/widgetReduce";
 import {
   getRowData,
   updateRowData,
   updateRowDataFirstRender,
-} from './rowDataFetch';
-import { FaSort, FaSortDown, FaSortUp } from 'react-icons/fa';
+} from "./rowDataFetch";
+import { FaSort, FaSortDown, FaSortUp } from "react-icons/fa";
 import {
   RowContainer,
   TableWrapper,
@@ -21,10 +20,10 @@ import {
   TableBody,
   BodyTable,
   HeaderTable,
-} from './styled';
-import TableRowComponent from './TableRowComponent';
-import { RowType, dataListType } from './types';
-
+} from "./styled";
+import TableRowComponent from "./TableRowComponent";
+import { RowType, dataListType } from "./types";
+import { sortDataByConfig } from "./util";
 const Row = ({
   firstTokenNameList,
   firstTokenDataList,
@@ -40,14 +39,18 @@ const Row = ({
   }>({});
   const [rowData, setRowData] = useState<{ [key: string]: dataListType }>({});
   const [sortConfig, setSortConfig] = useState({
-    key: '',
-    direction: '',
+    key: "",
+    direction: "",
   });
   const [fadeOutClass, setFadeOutClass] = useState({});
   const [expandedRow, setExpandedRow] = useState<string | null>(null);
 
   const dispatch: AppDispatch = useDispatch();
   const dollar = useSelector((state: RootState) => state.info.dollar);
+  const tether = useSelector((state: RootState) => state.info.tether);
+  const selectedCompareMarket = useSelector(
+    (state: RootState) => state.market.selectedCompareMarket
+  );
 
   const token = useSelector((state: RootState) => state.widget.token);
 
@@ -57,35 +60,10 @@ const Row = ({
     updateKimp(token);
   };
 
-  const sortDataByConfig = (
-    data: { [key: string]: dataListType },
-    key: string,
-    direction: string
-  ) => {
-    return Object.entries(data).sort((a, b) => {
-      const valueA = a[1][key];
-      const valueB = b[1][key];
-
-      if (key === 'kimp') {
-        const kimpA = a[1].secondPrice
-          ? (a[1].kimp + 1) * 100 // (kimp + 1) * 100 으로 퍼센트 값 계산
-          : -Infinity;
-        const kimpB = b[1].secondPrice ? (b[1].kimp + 1) * 100 : -Infinity;
-
-        return direction === 'asc' ? kimpA - kimpB : kimpB - kimpA;
-      }
-
-      if (!isFinite(valueA)) return 1;
-      if (!isFinite(valueB)) return -1;
-
-      return direction === 'asc' ? valueA - valueB : valueB - valueA;
-    });
-  };
-
   const sortData = (key: string) => {
-    let direction = 'desc';
-    if (sortConfig.key === key && sortConfig.direction === 'desc') {
-      direction = 'asc';
+    let direction = "desc";
+    if (sortConfig.key === key && sortConfig.direction === "desc") {
+      direction = "asc";
     }
 
     const sortedData = sortDataByConfig(rowData, key, direction);
@@ -126,26 +104,60 @@ const Row = ({
   useEffect(() => {
     if (
       nameList.length > 0 &&
-      dataList.length > 0 &&
+      firstTokenDataList &&
+      Object.keys(firstTokenDataList).length > 0 &&
       !Object.keys(rowData).length
     ) {
-      getRowData(nameList, dataList).then((initialRowData) => {
+      getRowData(nameList, firstTokenDataList).then((initialRowData) => {
         updateRowDataFirstRender(
           initialRowData,
           firstTokenDataList,
-          secondTokenDataList,
-          dollar
+          secondTokenDataList
         ).then((updatedData) => {
           setRowData(updatedData);
         });
       });
     }
-  }, [nameList, dataList]);
+  }, [
+    nameList,
+    firstTokenDataList,
+    secondTokenDataList,
+    dollar,
+    tether,
+    selectedCompareMarket,
+  ]);
+
+  // firstTokenDataList 또는 secondTokenDataList가 변경될 때 데이터 새로 로드
+  useEffect(() => {
+    if (
+      nameList.length > 0 &&
+      firstTokenDataList &&
+      secondTokenDataList &&
+      Object.keys(firstTokenDataList).length > 0
+    ) {
+      getRowData(nameList, firstTokenDataList).then((initialRowData) => {
+        updateRowDataFirstRender(
+          initialRowData,
+          firstTokenDataList,
+          secondTokenDataList
+        ).then((updatedData) => {
+          setRowData(updatedData);
+        });
+      });
+    }
+  }, [
+    firstTokenDataList,
+    secondTokenDataList,
+    nameList,
+    dollar,
+    tether,
+    selectedCompareMarket,
+  ]);
 
   useEffect(() => {
     if (rowData && firstDataset && secondTokenDataList) {
       setPrevRowData(rowData);
-      updateRowData(rowData, firstDataset, secondDataset, dollar).then(
+      updateRowData(rowData, firstDataset, secondDataset).then(
         (updatedData) => {
           // 정렬 상태가 있다면 정렬된 상태로 데이터 업데이트
           if (sortConfig.key) {
@@ -163,7 +175,7 @@ const Row = ({
             const prev = prevRowData[token]?.trade_price;
             const cur = firstDataset[token].trade_price;
             if (prev !== undefined && prev !== cur) {
-              newFadeOutClass[token] = 'fade-out';
+              newFadeOutClass[token] = "fade-out";
             }
           });
           setFadeOutClass(newFadeOutClass);
@@ -176,7 +188,7 @@ const Row = ({
     }
     updateTokenPrice(token);
     updateKimp(token);
-  }, [firstDataset]);
+  }, [firstDataset, dollar, tether, selectedCompareMarket]);
 
   return (
     <RowContainer>
@@ -187,13 +199,13 @@ const Row = ({
               <TableHeader>
                 <HeaderContent>코인</HeaderContent>
               </TableHeader>
-              <TableHeader onClick={() => sortData('trade_price')}>
+              <TableHeader onClick={() => sortData("trade_price")}>
                 <HeaderContent>현재가격</HeaderContent>
                 <SortButton
-                  className={sortConfig.key === 'trade_price' ? 'active' : ''}
+                  className={sortConfig.key === "trade_price" ? "active" : ""}
                 >
-                  {sortConfig.key === 'trade_price' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "trade_price" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -203,13 +215,13 @@ const Row = ({
                   )}
                 </SortButton>
               </TableHeader>
-              <TableHeader onClick={() => sortData('kimp')}>
+              <TableHeader onClick={() => sortData("kimp")}>
                 <HeaderContent>김프</HeaderContent>
                 <SortButton
-                  className={sortConfig.key === 'kimp' ? 'active' : ''}
+                  className={sortConfig.key === "kimp" ? "active" : ""}
                 >
-                  {sortConfig.key === 'kimp' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "kimp" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -219,13 +231,13 @@ const Row = ({
                   )}
                 </SortButton>
               </TableHeader>
-              <TableHeader onClick={() => sortData('change_rate')}>
+              <TableHeader onClick={() => sortData("change_rate")}>
                 <HeaderContent>변동률</HeaderContent>
                 <SortButton
-                  className={sortConfig.key === 'change_rate' ? 'active' : ''}
+                  className={sortConfig.key === "change_rate" ? "active" : ""}
                 >
-                  {sortConfig.key === 'change_rate' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "change_rate" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -235,13 +247,13 @@ const Row = ({
                   )}
                 </SortButton>
               </TableHeader>
-              <TableHeader onClick={() => sortData('highest_price')}>
+              <TableHeader onClick={() => sortData("highest_price")}>
                 <HeaderContent>52주 고가</HeaderContent>
                 <SortButton
-                  className={sortConfig.key === 'highest_price' ? 'active' : ''}
+                  className={sortConfig.key === "highest_price" ? "active" : ""}
                 >
-                  {sortConfig.key === 'highest_price' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "highest_price" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -251,13 +263,13 @@ const Row = ({
                   )}
                 </SortButton>
               </TableHeader>
-              <TableHeader onClick={() => sortData('lowest_price')}>
+              <TableHeader onClick={() => sortData("lowest_price")}>
                 <HeaderContent>52주 저가</HeaderContent>
                 <SortButton
-                  className={sortConfig.key === 'lowest_price' ? 'active' : ''}
+                  className={sortConfig.key === "lowest_price" ? "active" : ""}
                 >
-                  {sortConfig.key === 'lowest_price' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "lowest_price" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -267,15 +279,15 @@ const Row = ({
                   )}
                 </SortButton>
               </TableHeader>
-              <TableHeader onClick={() => sortData('acc_trade_price24')}>
+              <TableHeader onClick={() => sortData("acc_trade_price24")}>
                 <HeaderContent>누적 거래액</HeaderContent>
                 <SortButton
                   className={
-                    sortConfig.key === 'acc_trade_price24' ? 'active' : ''
+                    sortConfig.key === "acc_trade_price24" ? "active" : ""
                   }
                 >
-                  {sortConfig.key === 'acc_trade_price24' ? (
-                    sortConfig.direction === 'asc' ? (
+                  {sortConfig.key === "acc_trade_price24" ? (
+                    sortConfig.direction === "asc" ? (
                       <FaSortUp />
                     ) : (
                       <FaSortDown />
@@ -292,26 +304,21 @@ const Row = ({
         <TableBody>
           <BodyTable>
             <tbody>
-              {Object.entries(rowData)
-                .filter(([token]) => filteredTokens.includes(token))
-                .map(([token, data]) => (
-                  <TableRowComponent
-                    key={token}
-                    token={token}
-                    data={data}
-                    secondPrice={data.secondPrice}
-                    secondData={{
-                      token: token,
-                      trade_price: secondDataset[token]
-                        ? secondDataset[token].trade_price * dollar
-                        : 0,
-                    }}
-                    prevData={prevRowData[token]}
-                    expandedRow={expandedRow}
-                    fadeOutClass={fadeOutClass[token]}
-                    onRowClick={rowClick}
-                  />
-                ))}
+              {(() => {
+                return Object.entries(rowData)
+                  .filter(([token]) => filteredTokens.includes(token))
+                  .map(([token, data]) => (
+                    <TableRowComponent
+                      key={token}
+                      token={token}
+                      data={data}
+                      prevData={prevRowData[token]}
+                      expandedRow={expandedRow}
+                      fadeOutClass={fadeOutClass[token]}
+                      onRowClick={rowClick}
+                    />
+                  ));
+              })()}
             </tbody>
           </BodyTable>
         </TableBody>
