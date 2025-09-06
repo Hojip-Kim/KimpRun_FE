23a9074741dc28bfe87132e063072d6018ecff90
Hojip-Kim <rlaghwlq12@naver.com>
2025-09-06 18:32:23 +0900

    hotfix : 모바일뷰 scalable 설정을통한 확대/축소 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -142,6 +142,7 @@ export default function RootLayout({
           }}
         />
         
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       </head>
