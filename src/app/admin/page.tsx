151bff4b50d64e78417e6ec0447c1b8d99087690
Hojip-Kim <rlaghwlq12@naver.com>
2025-10-12 13:31:15 +0900

    feat : Admin page init (admin page 접속시 category page로 리다이렉트)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,39 +1,9 @@
-import React from 'react';
-import { fetchAllCategories } from './server/fetchCategory';
-import { ProcessedApiResponse } from '@/server/type';
-import { Category, CategoryResponse } from './type';
-import AdminPageClient from './client/adminClient';
+import { redirect } from 'next/navigation';
 
-// 동적렌더링 강제하여 빌드 시점에 로드되지 않도록 설정함
+// 동적렌더링 강제
 export const dynamic = 'force-dynamic';
 
-const AdminPage = async () => {
-  let initialCategories: Category[] = [];
-
-  try {
-    const categoriesResponse: ProcessedApiResponse<CategoryResponse> =
-      await fetchAllCategories();
-
-    if (
-      categoriesResponse &&
-      categoriesResponse.success &&
-      categoriesResponse.data
-    ) {
-      initialCategories = categoriesResponse.data.categories;
-    } else {
-      console.warn(
-        '카테고리 데이터를 가져올 수 없습니다:',
-        categoriesResponse?.error
-      );
-      initialCategories = [];
-    }
-  } catch (error) {
-    console.error('Admin 페이지 초기 데이터 로드 실패:', error);
-
-    initialCategories = [];
-  }
-
-  return <AdminPageClient initialCategories={initialCategories} />;
-};
-
-export default AdminPage;
+export default function AdminPage() {
+  // 기본 admin 페이지 접속 시 categories로 리다이렉트
+  redirect('/admin/categories');
+}
